823fe6f7649ddf420491343208532cfebf49f909
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2023-12-18 23:12:03 +0000

    Modified setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="pynamics",
+      version="0.1.0",
+      description="Python package intended to blend together control systems, machine learning, reinforcement learning, and optimisation algorithms.",
+      author="Miguel Santos Loureiro",
+      author_email="miguel.santos.loureiro@gmail.com",
+      packages=find_packages(["pynamics", "pynamics.*"]))
