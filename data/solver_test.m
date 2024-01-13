c73f2bc4b61aa8a2b546f040f3a64ddd80aa890e
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2024-01-13 21:50:22 +0000

    Added solver validation data files.

--- data/solver_test.m
+++ data/solver_test.m
@@ -13,7 +13,7 @@ C = [0, 0, 1];
 D = 0;
 
 % Define simulation parameters
-stop_time = "100.0";
+stop_time = "10.0";
 solver = ["ode1", "ode2", "ode4", "ode45"];
 fixed_step_solvers = 3;
 fixed_step_size = "0.001";
@@ -21,10 +21,11 @@ fixed = [true, true, true, false];
 results = zeros(str2double(stop_time) / str2double(fixed_step_size) + 1, ...
                 2 * fixed_step_solvers);
 
-% Set parameters and run simulations.
-test_model = "test_solver";
+% Set parameters and run simulations for 10 second simulations.
+test_model = "test_solver_2022b";
 load_system(test_model);
 set_param(test_model, "StopTime", stop_time);
+figure
 
 for i=1:length(solver)
 
@@ -41,13 +42,66 @@ for i=1:length(solver)
     end
     
     res_table = table(sim_results.tout, sim_results.y, ...
-                      "VariableNames", ["t", "y"]);
+                      'VariableNames', ["t", "y"]);
 
-    writetable(res_table, solver(i) + "_test.csv");
+    writetable(res_table, solver(i) + "_test_10sec.csv");
 
     plot(sim_results.tout, sim_results.y), hold on;
 
 end
 
-grid on, title("Simulation results"), xlabel("t (s)"), ylabel("y"), ...
+grid on, title("Simulation results - 10 seconds"), xlabel("t (s)"), ylabel("y"), ...
     legend("ode1", "ode2", "ode4", "ode45");
+
+% Compute errors for 10 second simulations.
+errors = [mean(abs(sim_results.y - results(:, 2))), ...
+          mean(abs(sim_results.y - results(:, 4))), ...
+          mean(abs(sim_results.y - results(:, 6)))];
+
+fprintf(1, "Mean absolute error for ode1: %f\n", errors(1));
+fprintf(1, "Mean absolute error for ode2: %f\n", errors(2));
+fprintf(1, "Mean absolute error for ode4: %f\n", errors(3));
+
+% Define simulation parameters for 100 second simulations.
+stop_time = "100.0";
+
+% Set parameters and run simulations for 100 second simulations.
+set_param(test_model, "StopTime", stop_time);
+results = zeros(str2double(stop_time) / str2double(fixed_step_size) + 1, ...
+                2 * fixed_step_solvers);
+figure
+
+for i=1:length(solver)
+
+    set_param(test_model, "Solver", solver(i));
+    
+    if(fixed(i) == true)
+    
+        set_param(test_model, "FixedStep", fixed_step_size);
+        sim_results = sim(test_model);
+    
+        results(:, i) = sim_results.tout;
+        results(:, i+1) = sim_results.y;
+
+    end
+    
+    res_table = table(sim_results.tout, sim_results.y, ...
+                      'VariableNames', ["t", "y"]);
+
+    writetable(res_table, solver(i) + "_test_100sec.csv");
+
+    plot(sim_results.tout, sim_results.y), hold on;
+
+end
+
+grid on, title("Simulation results - 100 seconds"), xlabel("t (s)"), ylabel("y"), ...
+    legend("ode1", "ode2", "ode4", "ode45");
+
+% Compute errors for 100 second simulations.
+errors = [mean(abs(sim_results.y - results(:, 2))), ...
+          mean(abs(sim_results.y - results(:, 4))), ...
+          mean(abs(sim_results.y - results(:, 6)))];
+
+fprintf(1, "Mean absolute error for ode1: %f\n", errors(1));
+fprintf(1, "Mean absolute error for ode2: %f\n", errors(2));
+fprintf(1, "Mean absolute error for ode4: %f\n", errors(3));
