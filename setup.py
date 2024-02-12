0ceef2bc928d75b17e70ded43bcb082b3ad1dfe5
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2024-02-12 00:04:46 +0000

    Restored defaults.

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name="pynamics",
       description="Python package intended to blend together control systems, machine learning, reinforcement learning, and optimisation algorithms.",
       author="Miguel Santos Loureiro",
       author_email="miguel.santos.loureiro@gmail.com",
-      #packages=find_packages(include=["pynamics", "pynamics.*"]),
-      packages=["pynamics", "pynamics.solvers", "pynamics.solvers.fixed_step"],
+      packages=find_packages(include=["pynamics", "pynamics.*"]),
+      #packages=["pynamics", "pynamics.solvers", "pynamics.solvers.fixed_step"],
       install_requires=["numpy>=1.26.3", "pandas>=2.1.4"])
