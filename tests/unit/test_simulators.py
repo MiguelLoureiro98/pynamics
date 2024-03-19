7721bdee649756a05737a9765558d395a4c73a3e
Miguel Santos Loureiro <miguel.santos.loureiro@gmail.com>
2024-03-19 16:28:52 +0000

    Created Simulator unit tests' file.

---
+++ tests/unit/test_simulators.py
@@ -0,0 +1,50 @@
+import unittest
+import numpy as np
+
+"""
+
+"""
+
+class TestSimulators(unittest.TestCase):
+
+    """
+    
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        
+        """
+        Unused.
+        """
+
+        pass
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        
+        """
+        Unused.
+        """
+
+        pass
+
+    def setUp(self) -> None:
+        
+        """
+        
+        """
+
+        pass
+
+    def tearDown(self) -> None:
+        
+        """
+        
+        """
+
+        pass
+
+if __name__ == "__main__":
+
+    unittest.main();
